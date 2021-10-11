29b1d4e5979874ef9dfb6c48d4c07a6225205d60
dzhandigova <dzhandigova.exceedteam@gmail.com>
2021-10-11 17:55:35 +0300

    Implemented sorting/filtering in application

---
+++ client/src/components/dateFilter/DateFilter.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { TextField } from '@material-ui/core';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import './index.scss';
+
+export const DateFilter = ({deleteFilter, setDatesPeriod}) => {
+
+  const [dateFrom, setDateFrom] = useState('');
+  const [dateTo, setDateTo] = useState('');
+
+  const sendFilterData = () => {
+    if (dateFrom && 
+        dateTo &&
+        dateFrom < dateTo) {
+      setDatesPeriod({dateFrom, dateTo});
+    }
+  }
+
+  return (
+    <div className='date-filter__container'>
+      <div className='date-filter__control'>
+        <label className='common-text'>
+          c :
+        </label>
+        <TextField
+          className='date-filter__field base-input'
+          type='date'
+          name='date'
+          defaultValue={null}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          InputProps={{
+            disableUnderline: true
+          }}
+          onChange={(e) => setDateFrom(e.target.value)}
+        />
+      </div>
+
+      <div className='date-filter__control'>
+        <label className='common-text'>
+          по :
+        </label>
+        <TextField
+          className='date-filter__field base-input'
+          type='date'
+          name='date'
+          InputLabelProps={{
+            shrink: true,
+          }}
+          InputProps={{
+            disableUnderline: true
+          }}
+          onChange={(e) => setDateTo(e.target.value)}
+        />
+      </div>
+
+      <button
+        className='btn-styled date-filter__confirm-btn'
+        onClick={sendFilterData}
+      >
+        Фильтровать
+      </button>
+
+      <DeleteOutlineIcon 
+        className='date-filter__cancel-btn'
+        onClick={() => deleteFilter(false)}
+      />
+    </div>
+  );
+}
---
+++ client/src/components/sortSelector/SortSelector.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.scss';
+
+export const SortSelector = ({ labelText, collection, changeFilterValue }) => {
+  return (
+    <div className='sort-selector__container'>
+      <label className='common-text'>{labelText}</label>
+      <select
+        className='sort-selector__field common-text base-input'
+        onChange={(e) => changeFilterValue(e.target.value)}
+      >
+        <option selected disabled hidden value=''></option>
+        {collection.map(collItem => {
+          return <option
+            key={collItem.id}
+            value={collItem.value}
+          >
+            {collItem.value}
+          </option>
+        })};
+      </select>
+    </div>
+  );
+}
